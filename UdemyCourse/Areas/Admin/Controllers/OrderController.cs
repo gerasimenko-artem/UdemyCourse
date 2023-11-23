96211256f2179fdb2d38302db24c9652d15d5ac9
ArtemGerasimenko <gerasimenko3452@gmail.com>
2023-11-23 10:37:05 +0200

    add Order Lsit

---
+++ UdemyCourse/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Udemy.DataAccess.Repository;
+using Udemy.DataAccess.Repository.IRepository;
+using Udemy.Models;
+using Udemy.Utility;
+
+namespace UdemyCourse.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = SD.Role_User_Admin)]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult OrderIndex()
+		{
+			return View();
+		}
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeader });
+		}
+		#endregion API CALLS
+
+	}
+}
---
+++ UdemyCourse/wwwroot/js/order.js
@@ -0,0 +1,28 @@
+﻿var dataTable
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "10%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+
+            {
+                data: 'orderId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group"> 
+                        <a href = "/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-fill"></i> Edit </a>
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+        
+    });
+}
